237a2a7ad1a8106aad16a747dc2ddd8b72e3c426
Matheus Henrique Fonseca Moreira <matheushfm0@gmail.com>
2026-04-21 03:30:15 -0300

    Adiciona script JS ao index.html

---
+++ portifolio.js
@@ -0,0 +1,9 @@
+/*Menu de navegação responsivo para dispositivos móveis: um botão (ex: ícone hambúrguer)
+que abre e fecha o menu ao ser clicado.
+
+Validação do formulário de contato: ao clicar em “Enviar”, verificar se os campos estão
+preenchidos e exibir uma mensagem de erro caso estejam vazios.*/
+alert('Opa está sem texto');
+/*Uma interação adicional à escolha da dupla. Exemplos: alternância entre tema claro e
+escuro (dark mode), carrossel de imagens, contador de caracteres no campo de mensagem,
+animação de entrada de elementos ao rolar a página, etc.*/
